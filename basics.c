46ef408d39986022892cf8b8c3618162a9697466
Giacomo Tocchetto <giacomo.tokez@gmail.com>
2025-04-08 15:25:10 +0200

    Add files via upload

---
+++ basics.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+typedef enum {PICCHE, FIORI, CUORI, QUADRI} Seme;
+typedef struct{
+    Seme seme;
+    int valore;
+    int stato;
+}carte;
+
+typedef struct{
+    char nome;
+    int vite;
+    
+}
+
+
+
+
+int main(){
+    
+}
